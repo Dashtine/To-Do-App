5f8cadc82891adbde16dceebf5814dc8f82ea91a
Dashtine Layson <123429496+Dashtine@users.noreply.github.com>
2023-11-05 20:33:30 -0800

    V1.1 Added Delete All button (#1)
    * V1.1 - Added Delete All button
    
    * V1.1 - Added Delete All Button

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
+/* Version 1.1 - 11/05/2023 - Added Delete All button           */
 /* Version 1.0 - 11/05/2023 - Initial build of the application  */
 /*==============================================================*/
 
 
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
+const btn_deleteAll = document.getElementById("deleteAll");
 
 function addTask(){
-    console.log("addTask()");
 
     if(inputBox.value === ''){
         alert("You must write something!");
@@ -18,10 +19,21 @@ function addTask(){
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
         li.appendChild(span);
+        btn_deleteAll.style.display = 'block';
     }
 
     inputBox.value = "";
     saveData();
+}
+
+function deleteAll(){
+
+    if (listContainer) {
+        listContainer.innerHTML = '';
+        btn_deleteAll.style.display = 'none';
+        saveData();
+    } 
+
 }
 
 listContainer.addEventListener("click", function(e){
